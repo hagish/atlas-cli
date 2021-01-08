9b560512b57a31f63ad1c457e8836dfe11e9fc3b
Sebastian Dorda <hagish@schattenkind.net>
2021-01-08 23:09:31 +0100

    Basic 2nd atlas generation

--- main.go
+++ main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"path/filepath"
 	"strings"
 )
 
-func Contains(s []string, str string) bool {
+func contains(s []string, str string) bool {
 	for _, v := range s {
 		if v == str {
 			return true
@@ -17,32 +18,63 @@ func Contains(s []string, str string) bool {
 	return false
 }
 
+func fileExists(filename string) bool {
+	info, err := os.Stat(filename)
+	if os.IsNotExist(err) {
+		return false
+	}
+	return !info.IsDir()
+}
+
 func main() {
 	root := "C:\\Users\\hagis\\projects1\\Colors-of-the-Forest\\Assets\\Generated\\Resources\\Biomes\\Campground"
 	outputDir := "C:\\Users\\hagis\\projects1\\Colors-of-the-Forest\\Assets\\Generated\\Resources\\Atlas\\Biomes_Campground"
 
 	files_cfill := ListPngFiles(root, "_cfill.png")
-	files_cudf := ListPngFiles(root, "_cudf.png")
 
-	var files_cfill_cleaned []string
-	var files_cudf_cleaned []string
+	maxSize := 2048
 
-	for _, s := range files_cfill {
-		other := strings.Replace(s, "_cfill.png", "_cudf.png", -1)
-		if Contains(files_cudf, other) {
-			files_cfill_cleaned = append(files_cfill_cleaned, s)
-		}
+	res, err := GenerateAtlas(files_cfill, outputDir, "atlas-cfill", maxSize)
+	if err != nil {
+		log.Fatal(err)
 	}
 
-	for _, s := range files_cudf {
-		other := strings.Replace(s, "_cudf.png", "_cfill.png", -1)
-		if Contains(files_cfill, other) {
-			files_cudf_cleaned = append(files_cudf_cleaned, s)
+	for i, atlas := range res.Atlases {
+		scale := 2
+
+		additionalAtlas := &Atlas{
+			Name:       fmt.Sprintf("%s-%d", "atlas-cdf", (i + 1)),
+			Width:      atlas.Width * scale,
+			Height:     atlas.Height * scale,
+			MaxWidth:   atlas.MaxWidth * scale,
+			MaxHeight:  atlas.MaxHeight * scale,
+			Descriptor: DESC_KIWI,
+			Padding:    0,
+			Gutter:     0,
+			Files:      make([]*File, 0),
+		}
+
+		// remap additional files
+		for _, file := range atlas.Files {
+			udfFile := strings.Replace(file.FileName, "_cfill.png", "_cudf.png", -1)
+			if fileExists(udfFile) {
+				additionalAtlas.Files = append(additionalAtlas.Files, &File{
+					X:        file.X * scale,
+					Y:        file.Y * scale,
+					FileName: udfFile,
+					Width:    file.Width * scale,
+					Height:   file.Height * scale,
+					Atlas:    additionalAtlas,
+				})
+			}
+		}
+
+		fmt.Printf("Writing atlas named %s to %s\n", additionalAtlas.Name, outputDir)
+		err = additionalAtlas.Write(outputDir)
+		if err != nil {
+			log.Fatal(err)
 		}
 	}
-
-	GenerateAtlas(files_cfill_cleaned, outputDir, "atlas-cfill",2048)
-	GenerateAtlas(files_cudf_cleaned, outputDir, "atlas-cudf",2048 * 2)
 }
 
 func ListPngFiles(root string, pattern string) []string {
@@ -62,21 +94,19 @@ func ListPngFiles(root string, pattern string) []string {
 	return files
 }
 
-func GenerateAtlas(files []string, outputDir string, name string, maxSize int) {
-	params := GenerateParams {
-		Name   	   : name, // The base name of the outputted files
-		Descriptor : DESC_KIWI, // The format of the data file for the atlases
-		Packer     : PackGrowing, // The algorithm to use when packing
-		Sorter	   : SortMaxSide, // The order to sort files by
-		MaxWidth   : maxSize, // Maximum width/height of the atlas images
-		MaxHeight  : maxSize,
-		MaxAtlases : 0, // Indicates no maximum
-		Padding    : 0, // The amount of blank space to add around each image
-		Gutter     : 0, // The amount to bleed the outer pixels of each image
+func GenerateAtlas(files []string, outputDir string, name string, maxSize int) (*GenerateResult, error) {
+	params := GenerateParams{
+		Name:       name,        // The base name of the outputted files
+		Descriptor: DESC_KIWI,   // The format of the data file for the atlases
+		Packer:     PackGrowing, // The algorithm to use when packing
+		Sorter:     SortMaxSide, // The order to sort files by
+		MaxWidth:   maxSize,     // Maximum width/height of the atlas images
+		MaxHeight:  maxSize,
+		MaxAtlases: 0, // Indicates no maximum
+		Padding:    0, // The amount of blank space to add around each image
+		Gutter:     0, // The amount to bleed the outer pixels of each image
 	}
 
-	_, err := Generate(files, outputDir, &params)
-	if err != nil {
-		log.Fatal(err)
-	}
+	res, err := Generate(files, outputDir, &params)
+	return res, err
 }
